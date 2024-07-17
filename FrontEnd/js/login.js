9c05f1f033aa420c0ab6725415f3d8c5b39416f3
Atef Lachhab <atef.lachhab1@gmail.com>
2024-07-17 09:38:56 -0400

    handleSubmit error and token handled

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -17,9 +17,14 @@ async function handleSubmit(event) {
     },
     body: JSON.stringify(user),
   });
-
+  if (response.status != 200) {
+    const errorBox = document.createElement("div");
+    errorBox.className = "error-login";
+    errorBox.innerHTML = "Il y a eu une erreur";
+    document.querySelector("form").prepend(errorBox);
+  }
   let result = await response.json();
-  console.log(result);
-  console.log("E-mail:", user.email);
-  console.log("Mot de passe:", user.password);
+  const token = result.token;
+  sessionStorage.setItem("authToken", token);
+  window.location.href = "index.html";
 }
