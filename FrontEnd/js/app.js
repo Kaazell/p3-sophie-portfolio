4cf24386d5961cd0865a22f59adb0ac009c8ddff
Atef Lachhab <atef.lachhab1@gmail.com>
2024-07-25 10:50:52 -0400

    login logout cleaned

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -97,6 +97,7 @@ function displayAdminMode() {
     editBanner.innerHTML =
       '<p><a href="#modal1" class="js-modal"><i class="fa-regular fa-pen-to-square"></i>Mode édition</a></p>';
     document.body.prepend(editBanner);
+    document.querySelector(".log-button").textContent = "logout";
   }
 }
 
